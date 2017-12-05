317e5a72a8da9251cc50f9141032e3efe6c959cf
LiangWei <lwchang9611@gmail.com>
2017-12-05 17:39:02 +0800

    add preprocess_data.py, utils.py

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cPickle as pickle
+import hickle
+import os
+
+def load_pickle(path):
+    with open(path, 'rb') as f:
+        file = pickle.load(f)
+        print ('Loaded %s..' %path)
+        return file
+
+def save_pickle(data, path):
+    with open(path, 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        print ('Saved %s..' %path)
