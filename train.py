01ffc220e7e6b98f4d48765a3ef98c12660a71dd
LiangWei <lwchang9611@gmail.com>
2017-12-06 12:37:47 +0800

    add train.py

---
+++ train.py
@@ -0,0 +1,22 @@
+import os
+import sys
+sys.path.append('utils')
+sys.path.append('model')
+
+from utils import load_dataset
+
+def main():
+    train_data = load_dataset('train')
+    word2idx = train_data['word2idx']
+    val_data = load_dataset('val')
+
+    model = CaptionGenerator(word2idx, dim_feature=[196, 512], dim_embed=512, dim_hidden=1024, n_time_step=16,
+                             prev2out=True, ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
+
+    solver = CaptioningSolver(model, data, val_data, n_epochs=20, batch_size=128, update_rule='adam', learning_rate=0.001, print_every=1000, save_every=1,
+                              image_path='./image/', pretrained_model=None, model_path='model/lstm/', test_model='model/lstm/model-10', print_bleu=True, log_path='log/')
+
+    solver.train()
+
+if __name__ == '__main__':
+    main()
--- utils/utils.py
+++ utils/utils.py
@@ -13,3 +13,18 @@ def save_pickle(data, path):
     with open(path, 'wb') as f:
         cPickle.dump(data, f)
         print ('Saved %s..' %path)
+
+def load_dataset(split):
+    data_path = os.path.join('./dataset', split)
+    data = {}
+
+    # Load Preprocessed Data
+    data['features'] = load_pickle(os.path.join(data_path, '%s_features.pkl' % split))
+    data['file_names'] = load_pickle(os.path.join(data_path, '%s_filenames.pkl' % split))
+    data['captions'] = load_pickle(os.path.join(data_path, '%s_captions.pkl' % split))
+    data['image_idxs'] = load_pickle(os.path.join(data_path, '%s_imageIdxs.pkl' % split))
+
+    if split == 'train':
+        data['word2idx'] = load_pickle(os.path.join(data_path, 'vocab.pkl'))
+
+    return data
